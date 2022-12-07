bb4bac30df23a4ce567af8eb61d6b767442bfe49
Samiha Sadek <samihasadek122@gmail.com>
2022-12-07 14:22:06 +0200

    Transform added

--- source/common/ecs/transform.cpp
+++ source/common/ecs/transform.cpp
@@ -9,8 +9,13 @@ namespace our {
     // Remember that the order of transformations is: Scaling, Rotation then Translation
     // HINT: to convert euler angles to a rotation matrix, you can use glm::yawPitchRoll
     glm::mat4 Transform::toMat4() const {
-        //TODO: (Req 3) Write this function
-        return glm::mat4(1.0f); 
+        
+        glm::mat4 Scaling_Matrix = glm::scale(glm::mat4(1.0f), scale);
+        glm::mat4 Rotation_Matrix = glm::yawPitchRoll(rotation.y, rotation.x, rotation.z);
+        glm::mat4 Translation_Matrix = glm::translate(glm::mat4(1.0f), position);
+
+        glm::mat4 Transformation_Matrix = Translation_Matrix * Rotation_Matrix * Scaling_Matrix;
+        return Transformation_Matrix; 
     }
 
      // Deserializes the entity data and components from a json object
